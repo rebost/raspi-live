4068649fe8b6d7c77e207d87aa1a1f3df8cceeaa
matias pizarro <matias@pizarro.net>
2019-03-24 00:30:11 +0100

    Test adding a rotation option

--- cli.js
+++ cli.js
@@ -30,14 +30,15 @@ program
   .option('-r, --framerate <fps>', 'video frames per second', Number, 25)
   .option('-x, --horizontal-flip', 'flip the camera horizontally')
   .option('-y, --vertical-flip', 'flip the camera vertically')
+  .option('-o, --rotation <rotation>', 'set image rotation [0-359]', range.bind(null, 0, 359), 0)
   .option('-c, --compression-level <compression-level>', 'compression level [0-9]', range.bind(null, 0, 9), 9)
   .option('-t, --time <time>', 'duration of streaming files', Number, 2)
   .option('-l, --list-size <list-size>', 'number of streaming files in the playlist', Number, 10)
   .option('-s, --storage-size <storage-size>', 'number of streaming files for storage purposes', Number, 10)
   .option('-p, --port <port>', 'port number the server runs on', Number, 8080)
-  .action(({ directory, format, width, height, framerate, horizontalFlip = false, verticalFlip = false, compressionLevel, time, listSize, storageSize, port }) => {
-    console.log('configuration:', directory, format, width, height, framerate, horizontalFlip, verticalFlip, compressionLevel, time, listSize, storageSize, port);
-    server(directory, format, width, height, framerate, horizontalFlip, verticalFlip, compressionLevel, time, listSize, storageSize, port);
+  .action(({ directory, format, width, height, framerate, horizontalFlip = false, verticalFlip = false, rotation,compressionLevel, time, listSize, storageSize, port }) => {
+    console.log('configuration:', directory, format, width, height, framerate, horizontalFlip, verticalFlip, rotation, compressionLevel, time, listSize, storageSize, port);
+    server(directory, format, width, height, framerate, horizontalFlip, verticalFlip, rotation, compressionLevel, time, listSize, storageSize, port);
   });
 
 program.parse(process.argv);
